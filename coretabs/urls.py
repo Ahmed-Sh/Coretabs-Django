82ed996fe40ff0c780216bc8d8d3691ef8850c11
Ahmed Shalaby <ahshalaby76@yahoo.com>
2018-04-27 16:34:33 +0200

    Adding views

--- coretabs/urls.py
+++ coretabs/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+rom . import  views
 
 urlpatterns = [
+    path('', views.index),
+    path('hello/<str:name>/', views.hello),
     path('admin/', admin.site.urls),
 ]
